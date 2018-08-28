f22d944fbff7e35c59e566e6298c23117d0237dc
xiaowu20121212 <1078336055@qq.com>
2018-08-28 11:06:38 +0800

    添加文件

---
+++ day_001/hello.py
@@ -0,0 +1,9 @@
+print('hello, world!')
+# print("你好,世界！")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
+number = [1, 2, 3, 4, 5]
+a, b, c, d, e = number
+print(a, b, c, d, e)
+print(*number)
