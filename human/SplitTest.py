b8099dbd0a7e2174868a15a821891b1b3a69a81d
小武20121212 <xiaowu2014>
2019-04-20 21:47:49 +0800

    微信

---
+++ human/SplitTest.py
@@ -0,0 +1,13 @@
+s = "fe,fad,s|fdef,ji<dfa,ds>"
+print(s.split(",", 2))
+from collections import Iterable, Iterator
+ll = "1234"
+i  = iter(ll)
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+i = iter(ll)
+print(next(i))
+dir(s.strip())
+
